8b661de470009c5d5b23ec6e8052b2c1e2bb68d1
lucascosm3 <lcds1944@gmail.com>
2024-05-15 07:43:05 -0300

    feat: introduction to (int) and (float) data type

---
+++ basic-programming-logic/class04-data-type-int-and-float.py
@@ -0,0 +1,20 @@
+# int and float types
+# int -> Integer number
+# The int type represents any positive or negative number.
+# An int without a sign is considered positive.
+# print(11) # int
+# print(-11) # int
+# print(0)
+
+# float -> Floating point number
+# The float type represents any positive or negative number
+# with a floating point.
+# A float without a sign is considered positive.
+# print(1.1, 10.11)
+# print(0.0, -1.5)
+
+# The type function shows the type that Python
+# inferred for the value.
+print(type('Lucas'))
+print(type(0)) # using class "type()" to identify the data type
+print(type(1.1), type(-1.1), type(0.0))
