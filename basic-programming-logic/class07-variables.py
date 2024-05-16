04007cf7690148f74ac707657a7d5d215ea47d95
lucascosm3 <lcds1944@gmail.com>
2024-05-16 09:00:15 -0300

    feat: use of variables

---
+++ basic-programming-logic/class07-variables.py
@@ -0,0 +1,17 @@
+# Variables are used to save something in the computer's memory.
+# PEP8: start variables with lowercase letters, you can use
+# numbers and underscores _.
+# The = sign is the assignment operator. It is used to
+# assign a value to a name (variable).
+# Usage: variable_name = expression
+#full_name = 'Lucas Cosme'
+#sum_two_plus_two = 2 + 2
+#print(type(full_name))
+#print(full_name, sum_two_plus_two)
+
+name = 'Lucas'
+age = 25
+of_legal_age = age >= 18
+print('Name:', name)
+print('Age:', age)
+print('of legal age?:', of_legal_age)
