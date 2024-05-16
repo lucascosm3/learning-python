d1fbf2781c8c1ef26cfb7e4e4a40663fb3aca880
lucascosm3 <lcds1944@gmail.com>
2024-05-16 09:00:03 -0300

    feat: use of typecasting

---
+++ basic-programming-logic/class06-type-convertion.py
@@ -0,0 +1,6 @@
+# Type conversion, typecasting, coercion
+# It is the act of converting one type to another
+# Immutable and primitive types:
+# str, int, float, bool
+print (int('5'), type(int('5')))
+print (float('1') + 1 )
