1dce3cd7c28929a0fa64c6e873861da9e88e497f
lucascosm3 <lcds1944@gmail.com>
2024-05-16 08:59:42 -0300

    feat: use of data type boolean

---
+++ basic-programming-logic/class05-data-type-boolean.py
@@ -0,0 +1,10 @@
+# Boolean data type
+# When asking a question in a program,
+# there are only two possible answers:
+# yes (True) or no (False).
+# There are several operators for "asking questions".
+# Among them is ==, which is a logical operator that
+# asks if one value is equal to another.
+print(10 == 10) # Yes => (True)
+print(10 == 11) # No => (False)
+print(type(10 > 11)) # type bool (boolean)
