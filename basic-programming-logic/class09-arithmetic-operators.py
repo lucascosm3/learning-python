46a37981ad538c298085af6ca25ec6053ac29f7f
lucascosm3 <lcds1944@gmail.com>
2024-05-17 07:33:48 -0300

    feat: examples arithmetic operators

---
+++ basic-programming-logic/class09-arithmetic-operators.py
@@ -0,0 +1,21 @@
+sum = 10 + 10
+print('Sum:' , sum)
+
+subtraction = 5 - 10
+print('Subtraction:' , subtraction)
+
+multiplication = 10 * 5
+print('Multiplication:' , multiplication)
+
+division = 10 / 2.2 #always returns result of type float
+print('Division:' , division)
+
+divison_int = 10 // 2.2 #always returns result of type int
+print('Division Int:' , divison_int)
+
+rest_division = 25 % 5
+print('Rest Division' , rest_division) 
+
+exponentiation = 2 ** 10
+print('Exponentiation:' , exponentiation)
+
