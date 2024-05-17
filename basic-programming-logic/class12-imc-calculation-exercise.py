0a815704766c3d41b93437f441b9c2277c4b205e
lucascosm3 <lcds1944@gmail.com>
2024-05-17 08:21:58 -0300

    feat: imc calculation exercise

--- basic-programming-logic/class12-imc-calculation-exercise.py
+++ basic-programming-logic/class12-imc-calculation-exercise.py
@@ -5,6 +5,6 @@ height = 1.71
 weight = 98.0
 imc = (weight // (height * 2))
 
-print(name, 'is', height, 'tall.')
+print(name, 'has', height, 'tall.')
 print('weighs', weight, 'kilos.' )
 print('Your IMC is:', imc)
