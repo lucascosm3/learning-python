dacb19cd8fa3af181aa5092fc46938c836d00fcc
lucascosm3 <lcds1944@gmail.com>
2024-05-17 08:21:15 -0300

    feat: imc calculation exercise

---
+++ basic-programming-logic/class12-imc-calculation-exercise.py
@@ -0,0 +1,10 @@
+# IMC Calculation
+
+name = 'Lucas Cosme'
+height = 1.71
+weight = 98.0
+imc = (weight // (height * 2))
+
+print(name, 'is', height, 'tall.')
+print('weighs', weight, 'kilos.' )
+print('Your IMC is:', imc)
