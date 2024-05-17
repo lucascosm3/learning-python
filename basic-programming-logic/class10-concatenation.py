fe87e37b9f8a16b10b05f226657d6edb3765446c
lucascosm3 <lcds1944@gmail.com>
2024-05-17 07:41:12 -0300

    feat: concatenation str & int

---
+++ basic-programming-logic/class10-concatenation.py
@@ -0,0 +1,7 @@
+concatenation = 'Lucas' + ' ' + 'Cosme'  # Just <str>
+print(concatenation)
+
+repetition_10_times = 'A' * 10 # just str + int
+repetition_3_times = 'Lucas ' * 3
+print(repetition_10_times)
+print(repetition_3_times)
