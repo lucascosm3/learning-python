c95886714fb685aac674a7ab515d4787fbbe448e
lucascosm3 <lcds1944@gmail.com>
2024-05-17 07:07:01 -0300

    feat: exercise use of variables,int,floats,boolean

---
+++ basic-programming-logic/class08-variable-exercise.py
@@ -0,0 +1,14 @@
+name = 'Lucas'
+surname = 'Cosme'
+current_year = 2024
+year_of_birth = 1999
+age = (current_year - year_of_birth)
+of_legal_age = (current_year - year_of_birth) >= (18)
+height = 1.71
+
+print('Name:' , name)
+print('Surname:' , surname)
+print('Age:' , age)
+print('Year of Birth:' , year_of_birth)
+print('Of legal age?:' , of_legal_age)
+print('Height' , height )
