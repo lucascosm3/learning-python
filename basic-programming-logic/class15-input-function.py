928436f7347089d0fbeabdcc9150d597482e8455
lucascosm3 <lcds1944@gmail.com>
2024-05-20 07:23:22 -0300

    feat: input function

---
+++ basic-programming-logic/class15-input-function.py
@@ -0,0 +1,7 @@
+number_1 = input('Enter a number: ')
+number_2 = input('Enter another number: ')
+
+int_number_1 = int(number_1)
+int_number_2 = int(number_2)
+
+print(f'The sum of the numbers is: {int_number_1 + int_number_2}')
