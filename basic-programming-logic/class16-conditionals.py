5082e27b68f1d2562c9591ddb70cb1b85d2bbf84
lucascosm3 <lcds1944@gmail.com>
2024-05-20 07:47:44 -0300

    feat: working with conditionals

---
+++ basic-programming-logic/class16-conditionals.py
@@ -0,0 +1,12 @@
+# if / elif / else
+
+input = input('Do you want to "enter" or "exit"?: ' )
+
+if input == 'enter':
+    print('You entered the system!')
+
+elif input == 'exit':
+    print('You left the system!')
+
+else:
+    print('You didnt type enter or exit! ')
