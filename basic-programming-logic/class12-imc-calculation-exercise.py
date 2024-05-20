61c786688b30a45c2b9a9c76e14af8c8f5d8635f
lucascosm3 <lcds1944@gmail.com>
2024-05-20 06:47:20 -0300

    feat: IMC exercise

--- basic-programming-logic/class12-imc-calculation-exercise.py
+++ basic-programming-logic/class12-imc-calculation-exercise.py
@@ -5,9 +5,6 @@ height = 1.71
 weight = 98.0
 imc = (weight // (height * 2))
 
-f_strings = f'Name: {name} \nHeight: {height:.2f} \nWeighs {weight} kilos \nIMC: {imc}'
-print(f_strings)
-
-#print(name, 'has', height, 'tall.')
-#print('weighs', weight, 'kilos.' )
-#print('Your IMC is:', imc)
+print(name, 'has', height, 'tall.')
+print('weighs', weight, 'kilos.' )
+print('Your IMC is:', imc)
