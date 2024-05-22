4301aa3f7f03dfe24f545a87378b0c17ae422392
lucascosm3 <lcds1944@gmail.com>
2024-05-22 07:17:58 -0300

    feat: using debug

---
+++ basic-programming-logic/class18-debug.py
@@ -0,0 +1,24 @@
+# if / elif      / else
+# if / else if / else
+
+condition1 = True
+condition2 = True
+condition3 = True
+condition4 = True
+
+if condition1:
+    print('Code for condition 1')
+    print('Code for condition 1')
+elif condition2:
+    print('Code for condition 2')
+elif condition3:
+    print('Code for condition 3')
+elif condition4:
+    print('Code for condition 4')
+else:
+    print('No condition was satisfied.')
+
+if 10 == 10:
+    print('Another if')
+
+print('Outside of if')
