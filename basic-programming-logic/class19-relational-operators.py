89af875196bcc84af32f0edea41d83ebf06c9ea4
lucascosm3 <lcds1944@gmail.com>
2024-05-22 07:29:53 -0300

    feat: relational operators

---
+++ basic-programming-logic/class19-relational-operators.py
@@ -0,0 +1,10 @@
+"""
+Comparison (Relational) Operators
+OP      Meaning             Example (True)
+>       greater             2 > 1
+>=      greater or equal    2 >= 2
+<       less                1 < 2
+<=      less or equal       2 <= 2
+==      equal               'a' == 'a'
+!=      different           'a' != 'b'
+"""
